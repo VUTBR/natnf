60c010dadc82341ed53b193515b97709d1f9c118
iK.sSs <psychiksss@gmail.com>
2017-05-02 14:33:51 +0200

    Parse NAT events Print out NAT translate BUG on line 121 - dest_ip dont copy in repl

--- main.c
+++ main.c
@@ -5,21 +5,60 @@
 
 #include <libnetfilter_conntrack/libnetfilter_conntrack.h>
 
+#define EVENTS_UNLIMITED
 #define EVENTS_MAX 10
 
-#ifdef EVENTS_UNLIMITED
-# define EVENTS_DONE(cnt) (cnt == EVENTS_MAX)
+#ifndef EVENTS_UNLIMITED
+# define EVENTS_DONE(cnt) (cnt >= EVENTS_MAX)
 #else
 # define EVENTS_DONE(cnt) (0)
 #endif
 
+const char S_SRC_IP[]	= "src=";
+const char S_DST_IP[]	= "dst=";
+const char S_SRC_PORT[]	= "sport=";
+const char S_DST_PORT[]	= "dport=";
+
+enum direction {
+	LOCAL_OUT,
+	LOCAL_IN
+};
+
+struct nat_ip_port {
+	char src_ip[16];
+	char src_port[6];
+    char dst_ip[16];
+    char dst_port[6];
+};
+
 static int event_cb(enum nf_conntrack_msg_type type,
-		    struct nf_conntrack *ct,
-		    void *data)
+					struct nf_conntrack *ct,
+					void *data)
 {
 	static int counter = 0;
-    int is_nat;
+	int is_nat;
 	char buf[1024];
+	struct nf_conntrack *ct_orig, *ct_repl;
+	struct nat_ip_port *nat;
+
+	ct_orig = nfct_new();
+	if (!ct_orig) {
+		perror("nfct_new");
+		return 0;
+	}
+
+	ct_repl = nfct_new();
+	if (!ct_repl) {
+		perror("nfct_new");
+		return 0;
+	}
+
+	nat = (struct nat_ip_port*)malloc(sizeof(struct nat_ip_port));
+
+	if ( NFCT_T_UPDATE == type )
+	{
+		return NFCT_CB_CONTINUE;
+	}
 
     /* We're only interested in source NAT. */
     is_nat = nfct_getobjopt(ct, NFCT_GOPT_IS_SNAT) ||
@@ -30,14 +69,129 @@ static int event_cb(enum nf_conntrack_msg_type type,
         return NFCT_CB_CONTINUE;
     }
 
+    nfct_copy(ct_orig, ct, NFCT_CP_ORIG);
+    nfct_copy(ct_repl, ct, NFCT_CP_REPL);
+
 	nfct_snprintf(buf, sizeof(buf), ct, type, NFCT_O_DEFAULT, NFCT_OF_TIME | NFCT_OF_TIMESTAMP);
 	printf("%s\n", buf);
+
+	parse_nat_header(nat, ct_orig, type, LOCAL_OUT);
+	print_nat_header(nat, LOCAL_OUT);
+
+	parse_nat_header(nat, ct_repl, type, LOCAL_IN);
+	print_nat_header(nat, LOCAL_IN);
 
     counter++;
 	if (EVENTS_DONE(counter))
 		return NFCT_CB_STOP;
 
 	return NFCT_CB_CONTINUE;
+}
+
+void parse_nat_header (	struct nat_ip_port *nat, 
+						struct nf_conntrack *ct,
+						enum nf_conntrack_msg_type type,
+						enum direction dir)
+{
+	char buf[1024];
+
+	char *start, *end;
+
+	nfct_snprintf(buf, sizeof(buf), ct, type, NFCT_O_DEFAULT, NFCT_OF_TIME | NFCT_OF_TIMESTAMP);
+
+	//ensure NULL string if parse fail
+	nat->src_ip[0]	= 0;
+	nat->src_port[0]= 0;
+	nat->dst_ip[0]	= 0;
+	nat->dst_port[0]= 0;
+
+	start = strstr(buf, S_SRC_IP);
+	if ( NULL != start )
+	{
+		start += sizeof S_SRC_IP - 1;	// length of "src="
+		end = strchr(start,' ');
+
+		if ( LOCAL_OUT == dir )
+		{
+			strncpy(nat->src_ip, start, end-start);
+			nat->src_ip[end-start+1] = 0;
+		}
+		else
+		{
+			//printf("\nDST IP: %.*s\n", end-start, start);
+			strncpy(nat->dst_ip, start, end-start);
+			nat->dst_ip[end-start+1] = 0;
+		}
+	}
+
+	start = strstr(buf, S_SRC_PORT);
+	if ( NULL != start )
+	{
+		start += sizeof S_SRC_PORT - 1;	// length of "sport="
+		end = strchr(start,' ');
+
+		if ( LOCAL_OUT == dir )
+		{
+			strncpy(nat->src_port, start, end-start);
+			nat->src_port[end-start+1] = 0;
+		}
+		else
+		{
+			strncpy(nat->dst_port, start, end-start);
+			nat->dst_port[end-start+1] = 0;
+		}
+	}
+
+	start = strstr(buf, S_DST_IP);
+	if ( NULL != start )
+	{
+		start += sizeof S_DST_IP - 1;	// length of "dst="
+		end = strchr(start,' ');
+
+		if ( LOCAL_OUT == dir )
+		{
+			strncpy(nat->dst_ip, start, end-start);
+			nat->dst_ip[end-start+1] = 0;
+		}
+		else
+		{
+			strncpy(nat->src_ip, start, end-start);
+			nat->src_ip[end-start+1] = 0;
+		}
+	}
+
+	start = strstr(buf, S_DST_PORT);
+	if ( NULL != start )
+	{
+		start += sizeof S_DST_PORT - 1;	// length of "dport="
+		end = strchr(start,' ');
+
+		if ( LOCAL_OUT == dir )
+		{
+			strncpy(nat->dst_port, start, 5);	//till end of string
+			nat->dst_port[6] = 0;
+		}
+		else
+		{
+			strncpy(nat->src_port, start, 5);	//till end of string
+			nat->src_port[6] = 0;
+		}
+	}
+
+	return;
+}
+
+void print_nat_header (	struct nat_ip_port *nat,
+						enum direction dir)
+{
+	if ( LOCAL_OUT == dir )
+	{
+		printf("%s:%s %s:%s\t", nat->src_ip, nat->src_port, nat->dst_ip, nat->dst_port);
+	}
+	else
+	{
+		printf("-->\t%s:%s %s:%s\n", nat->src_ip, nat->src_port, nat->dst_ip, nat->dst_port);
+	}
 }
 
 int main(void)
